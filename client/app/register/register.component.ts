5209c5237ce9950649aa23c6370851a70524aa51
mtmulch <mtmulch@gmail.com>
2017-03-08 14:45:59 -0500

    heroku database and heroku autodeploy

--- client/app/register/register.component.ts
+++ client/app/register/register.component.ts
@@ -9,5 +9,10 @@ import { Component } from "@angular/core";
 })
 
 export class RegisterComponent {
+    firstName: string = "";
+    password: string = "";
 
+    update() {
+        console.log(this.firstName, this.password);
+    }
 }
