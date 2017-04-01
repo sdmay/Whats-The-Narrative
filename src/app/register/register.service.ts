11f168c1ad404faaeea18b65629a602ff92e7581
Michael Mulchrone <mtmulch@gmail.com>
2017-04-01 09:47:09 -0400

    register component

---
+++ src/app/register/register.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class RegisterService {
+    constructor(private http: Http) {}
+
+    registerUser(userObject): Observable<any> {
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        const options = new RequestOptions({ headers: headers });
+        return this.http.post('api/userauth/registeruser', userObject, options)
+            .map(this.extractRegisterUserData)
+            .catch(this.handleError);
+    }
+
+    extractRegisterUserData(res: Response): void {
+        const body = res.json();
+        console.log(body);
+    }
+
+     public handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+
+}
